4d6c1afe37de0afcf7d8da2540dce35dd59dbf3e
nicolas.espinosa <nicolas.espinosa@primestone.com>
2021-05-26 06:32:52 -0500

    -NE: Implementación de relaciones

--- project/composers/student_compose.py
+++ project/composers/student_compose.py
@@ -5,6 +5,22 @@ class StudentCompose(Compose):
     def __init__(self, database_cache_repository, sender_queue_service_client):
         self.repository = database_cache_repository
 
+    def associate_student_course(self, data_structure):
+        return self.repository.insert(values=data_structure,
+                                      entity_name="studentCourse")
+
+    def associate_student_address(self, data_structure):
+        return self.repository.insert(values=data_structure,
+                                      entity_name="studentAddress")
+
+    def disassociate_student_course(self, data_structure):
+        return self.repository.delete(id=data_structure["identifier"],
+                                      entity_name="studentCourse")
+
+    def disassociate_student_address(self, data_structure):
+        return self.repository.delete(id=data_structure["identifier"],
+                                      entity_name="studentAddress")
+
     def get_student(self):
         return self.repository.select_all(entity_name="student")
 
